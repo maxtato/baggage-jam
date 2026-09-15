cb1116214bd33a3fa680b48012ecdc29a43fd724
maxtato <43971990+maxtato@users.noreply.github.com>
2026-09-15 08:41:10 +0200

    Remove baggage handler arm and hand

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "baggage-jam-v9-baggage-handler";
+const CACHE = "baggage-jam-v10-no-handler";
 const SHELL = ["/", "/index.html", "/apple-icon.png", "/manifest.json"];
 
 self.addEventListener("install", (e) => {
